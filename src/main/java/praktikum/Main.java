8ea1f16709ed0d098f7591391e514a453419fd7d
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2024-07-29 18:35:52 +0200

    fix spelling

--- src/main/java/praktikum/Main.java
+++ src/main/java/praktikum/Main.java
@@ -3,6 +3,7 @@ package praktikum;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main world");
+        System.out.println("NIce and shiny");
         System.out.println("May there be conflict");
     }
 }
